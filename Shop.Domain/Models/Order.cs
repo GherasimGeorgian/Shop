fdc89507a3c981e07e4b37df30407709f0e96826
Geo <Geo@DESKTOP-4HDFJN5>
2020-09-21 19:16:45 +0300

    Customer Information

--- Shop.Domain/Models/Order.cs
+++ Shop.Domain/Models/Order.cs
@@ -8,6 +8,12 @@ namespace Shop.Domain.Models
     {
         public int Id { get; set; }
         public string OrderRef { get; set; }
+     
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
         public string Address1 { get; set; }
         public string Address2 { get; set; }
         public string City { get; set; }
