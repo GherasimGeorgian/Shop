b21aa2be9a0fe4fc29c87112dccc2908c9b02303
Geo <Geo@DESKTOP-4HDFJN5>
2020-09-21 16:53:38 +0300

    Cart View Component

---
+++ Shop.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Cart;
+using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.UI.ViewComponents
+{
+    public class CartViewComponent:ViewComponent
+    {
+        private ApplicationDbContext _context;
+        public CartViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke(string view = "Default")
+        {
+         
+            return View(view,new GetCart(HttpContext.Session,_context).Do());
+        }
+    }
+}
